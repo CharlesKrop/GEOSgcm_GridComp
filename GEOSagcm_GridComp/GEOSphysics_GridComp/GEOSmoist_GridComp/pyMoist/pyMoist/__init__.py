1b4300dd85ef1ccadd85b21ac8ee6e0b1a6c115f
Charles Kropiewnicki <charles.j.krop@gmail.com>
2026-05-07 16:20:26 -0400

    improved __init__.py

--- GEOSagcm_GridComp/GEOSphysics_GridComp/GEOSmoist_GridComp/pyMoist/pyMoist/__init__.py
+++ GEOSagcm_GridComp/GEOSphysics_GridComp/GEOSmoist_GridComp/pyMoist/pyMoist/__init__.py
@@ -1,8 +1,14 @@
 from pyMoist.microphysics import GFDL1M, GFDL1MConfig, GFDL1MState
-
+from pyMoist.convection import GF2020, GF2020Config, GF2020State, UWState, UWConfiguration, ComputeUwshcuInv
 
 __all__ = [
     "GFDL1M",
     "GFDL1MState",
     "GFDL1MConfig",
+    "GF2020",
+    "GF2020Config",
+    "GF2020State",
+    "UWState",
+    "UWConfiguration",
+    "ComputeUwshcuInv",
 ]
---
+++ GEOSagcm_GridComp/GEOSphysics_GridComp/GEOSmoist_GridComp/pyMoist/pyMoist/convection/__init__.py
@@ -0,0 +1,17 @@
+from pyMoist.convection.GF_2020 import GF2020, GF2020Config, GF2020State
+from pyMoist.convection.UW import ComputeUwshcuInv, UWConfiguration, UWState
+from pyMoist.config import MoistConfig
+from pyMoist.moist import Moist
+from pyMoist.state import MoistState
+
+__all__ = [
+    "MoistConfig",
+    "Moist",
+    "MoistState",
+    "GF2020",
+    "GF2020Config",
+    "GF2020State",
+    "ComputeUwshcuInv",
+    "UWConfiguration",
+    "UWState",
+]
