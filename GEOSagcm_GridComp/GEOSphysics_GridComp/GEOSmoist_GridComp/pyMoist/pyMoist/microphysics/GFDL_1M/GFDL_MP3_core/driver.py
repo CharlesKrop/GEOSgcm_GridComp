ca2d65a535787408d1d5504f9c162137c2ba729d
Charles Kropiewnicki <charles.j.krop@gmail.com>
2026-08-27 17:31:12 -0400

    created location for the new driver for v11.10 GFDL1M

---
+++ GEOSagcm_GridComp/GEOSphysics_GridComp/GEOSmoist_GridComp/pyMoist/pyMoist/microphysics/GFDL_1M/GFDL_MP3_core/driver.py
@@ -0,0 +1,21 @@
+class GFDL_MP3_Driver:
+    """GFDL Cloud Microphysics Package (GFDL MP) Version 3
+    The algorithms are originally derived from Lin et al. (1983).
+    Most of the key elements have been simplified / improved.
+    This code at this stage bears little to no similarity to the original Lin MP in ZETAC.
+    Developers: Linjiong Zhou and the GFDL FV3 Team
+    References:
+    Version 0: Chen and Lin (2011 doi: 10.1029/2011GL047629, 2013 doi: 10.1175/JCLI-D-12-00061.1)
+    Version 1: Zhou et al. (2019 doi: 10.1175/BAMS-D-17-0246.1)
+    Version 2: Harris et al. (2020 doi: 10.1029/2020MS002223), Zhou et al. (2022 doi: 10.25923/pz3c-8b96)
+    Version 3: Zhou et al. (2022 doi: 10.1029/2021MS002971)
+    NASA integration: Putman April 2025
+    NDSL integration: Kropiewnicki August 2026
+    """
+
+    def __init__(self):
+        pass
+
+    def __call__(self, *args, **kwds):
+        pass
+    
