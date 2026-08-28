de275522d41904f66c400f79346d3aac24eb13e3
Charles Kropiewnicki <charles.j.krop@gmail.com>
2026-08-28 16:40:09 -0400

    removing unused imports

--- GEOSagcm_GridComp/GEOSphysics_GridComp/GEOSmoist_GridComp/pyMoist/pyMoist/microphysics/GFDL_1M/setup.py
+++ GEOSagcm_GridComp/GEOSphysics_GridComp/GEOSmoist_GridComp/pyMoist/pyMoist/microphysics/GFDL_1M/setup.py
@@ -1,17 +1,14 @@
 import dataclasses
 
-from ndsl import Local, LocalState, NDSLRuntime, Quantity, QuantityFactory, StencilFactory
+from ndsl import Local, LocalState, NDSLRuntime, QuantityFactory, StencilFactory
 from ndsl.constants import I_DIM, J_DIM, K_DIM, K_INTERFACE_DIM
 from ndsl.dsl.gt4py import BACKWARD, FORWARD, PARALLEL, K, computation, function, interval, log
 from ndsl.dsl.typing import BoolFieldIJ, Float, FloatField, FloatFieldIJ, IntFieldIJ, Int
 from ndsl.stencils.basic_operations import set_value, copy, add
 
-from pyMoist.constants import MAPL_ALHL, MAPL_CP, MAPL_CPDRY, MAPL_CPVAP, MAPL_GRAV, MAPL_KAPPA, MAPL_P00, MAPL_RGAS, MAPL_RVAP
+from pyMoist.constants import MAPL_CPDRY, MAPL_CPVAP, MAPL_GRAV, MAPL_RGAS, MAPL_RVAP
 from pyMoist.microphysics.GFDL_1M.config import GFDL1MConfig
-from pyMoist.microphysics.GFDL_1M.shared_stencils import prepare_tendencies
 from pyMoist.saturation_tables import GlobalTable_saturation_tables, SaturationVaporPressureTable, saturation_specific_humidity
-from pyMoist.shared.interpolations import vertical_interpolation
-from pyMoist.shared.atmos_recipes import get_fac_eis
 from pyMoist.microphysics.GFDL_1M.locals import GFDL1MLocals
 from pyMoist.microphysics.GFDL_1M.state import GFDL1MState
 
